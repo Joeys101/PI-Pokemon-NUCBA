b1c1eba8dc7ca402aa521b3230f9caa8d6c1e701
Joeys101 <jose.onepice@hotmail.com>
2022-08-09 19:41:32 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,73 @@
+let pokemons = {
+    data: [
+        {
+            nombre: "bulbasaur",
+            id: 0,
+            tipo: "Starter",
+            precio: "$1.000",
+            image:"./pokemon/bulbasaur.png"
+        },
+        {
+            nombre: "squirtle",
+            id: 1,
+            tipo: "Starter",
+            precio: "$1.000",
+            image:"./pokemon/squirtle.png"
+        },
+        {
+            nombre: "charmander",
+            id: 2,
+            tipo: "Starter",
+            precio: "$1.000",
+            image:"./pokemon/charmander.png"
+        },
+        {
+            nombre: "treecko",
+            id: 3,
+            tipo: "Starter",
+            precio: "$2.000",
+            image:"./pokemon/treecko.png"
+        },
+        {
+            nombre: "mudkip",
+            id: 4,
+            tipo: "Starter",
+            precio: "$1.700",
+            image:"./pokemon/mudkip.png"
+        },
+        {
+            nombre: "torchic",
+            id: 5,
+            tipo: "Starter",
+            precio: "$1.500",
+            image:"./pokemon/torchic.png"
+        },
+        {
+            nombre: "kyogre",
+            id: 6,
+            tipo: "Legendario",
+            precio: "$19.000",
+            image:"./pokemon/kyogre.png"
+        },
+        {
+            nombre: "zekrom",
+            id: 7,
+            tipo: "Legendario",
+            precio: "$25.000",
+            image:"./pokemon/zekrom.png"
+        },
+        {
+            nombre: "darkrai",
+            id: 8,
+            tipo: "Starter",
+            precio: "$50.000",
+            image:"./pokemon/darkrai.png"
+        },
+    
+        
+    ]
+}
+
+for (i of pokemons.data) {
+    let card = document.createElement("div");
+}
