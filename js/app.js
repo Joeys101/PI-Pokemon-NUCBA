d9d983aef340ade62fc96baf7d616cf2b099e886
Joeys101 <jose.onepice@hotmail.com>
2022-11-20 14:43:08 -0300

    Carrito de compras

--- js/app.js
+++ js/app.js
@@ -168,3 +168,24 @@ document.addEventListener("keyup", e=>{
 
 //Carrito de compras
 
+//Funcion abrir y cerrar carrito
+
+function closeCart() {
+	const cart = document.querySelector('.producstOnCart');
+	cart.classList.toggle('hide');
+	document.querySelector('body').classList.toggle('stopScrolling')
+}
+
+
+const openShopCart = document.querySelector('.shoppingCartButton');
+openShopCart.addEventListener('click', () => {
+	const cart = document.querySelector('.producstOnCart');
+	cart.classList.toggle('hide');
+	document.querySelector('body').classList.toggle('stopScrolling');
+});
+
+
+const closeShopCart = document.querySelector('#closeButton');
+const overlay = document.querySelector('.overlay');
+closeShopCart.addEventListener('click', closeCart);
+overlay.addEventListener('click', closeCart);
